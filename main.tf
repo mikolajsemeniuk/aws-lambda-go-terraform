5352bb30b874c7274bf0d629518ed1ed1b44a477
Mikolaj Semeniuk <m.semeniuk@icloud.com>
2023-08-19 14:37:45 +0200

    Update terraform

--- main.tf
+++ main.tf
@@ -33,25 +33,30 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
   retention_in_days = 7
 }
 
-resource "aws_iam_role_policy" "lambda_cloudwatch_policy" {
-  name = "LambdaCloudWatchPolicy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect   = "Allow",
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# resource "aws_iam_role_policy" "lambda_cloudwatch_policy" {
+#   name = "LambdaCloudWatchPolicy"
+#   role = aws_iam_role.lambda_role.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         Effect   = "Allow",
+#         Resource = "arn:aws:logs:*:*:*"
+#       }
+#     ]
+#   })
+# }
 
 data "archive_file" "lambda_function" {
   type        = "zip"
